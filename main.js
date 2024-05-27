05903ef9eccd6f04a187d8283bcd1f0db41114ba
ORB-E <brayamortegarenteria@gmail.com>
2024-05-27 13:58:29 -0600

    feat(aimation): text render with animation

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const content = document.getElementById('content')
 
 const text = 'Hola mundo'
-
-content.textContent=text
+let i=0
+const lenght = text.length
+const interval = setInterval(()=>{
+    content.textContent+=text[i++]
+    if(i>=lenght) clearInterval(interval)
+},100)
