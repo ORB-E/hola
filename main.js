5e0e6c5686720b4e9f9f7992a9636ceac3f684ab
ORB-E <brayamortegarenteria@gmail.com>
2024-05-27 14:22:34 -0600

    feat(random): random animation for text

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const content = document.getElementById('content')
 
-const text = content.textContent
-content.textContent=''
-let i=0
-const lenght = text.length
-const interval = setInterval(()=>{
-    content.textContent+=text[i++]
-    if(i>=lenght) clearInterval(interval)
-},100)
+
+async function animation(){
+    const text = content.textContent
+    content.textContent=''
+    const lenght = text.length
+    for(let i=0;i<lenght;i++){
+        let char = ''
+        const txt = content.textContent
+        await new Promise((resolve)=>{
+            const interval = setInterval(()=>{
+                char=String.fromCharCode(random(32,122))
+                content.textContent=txt+char
+                console.log(char==text[i],text,i)
+                if(char==text[i]){
+                    clearInterval(interval)
+                    resolve()
+                }
+            },10)
+        })
+    }
+}
+
+function random(min,max){
+    max++
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+animation()
